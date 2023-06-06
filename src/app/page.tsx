a86cefc13b3dd7021a3916c8de8173da48f43996
matheusmntt <matheus.monteiro7@outlook.com>
2023-06-06 00:22:19 -0300

    Projeto carrinho de compras Rockeseat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,135 @@
-import Image from 'next/image'
+'use client'
+
+import { X, Minus, Plus, Tag } from '@phosphor-icons/react';
+import monitor from '../assets/monitor.svg'
+import cadeira from '../assets/cadeira.svg'
+import teclado from '../assets/teclado.svg'
+import headset from '../assets/headset.svg'
+import patinho from '../assets/patinho.svg'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-[428px] bg-surface-color">
+      <header className="h-[72px] flex justify-around items-center border-b-2 border-stroke-color">
+        <h1 className="py-6 text-color-primary">Seu carrinho tem <strong>5 itens</strong></h1>
+        <X size={24} className='text-close-button-color hover:text-color-primary cursor-pointer'/>
+      </header>
+
+      <main className='h-[506px] p-8 gap-12 grid overflow-y-scroll'>
+        <div className='flex justify-between items-center gap-6'>
+          <Image src={monitor} width={104} height={104} alt='Monitor Gamer' className='object-cover rounded-lg'/>
+          <div className='w-[232px] gap-6 flex flex-col'>
+            <div>
+              <p className='text-color-primary'>Monitor Gamer Curvo 49 DQHD, 240Hz, 1ms, HDMI e... </p>
+            </div>
+            <div className='flex justify-between items-center'>
+              <span className='text-color-primary'><strong>R$8.599,90</strong></span>
+              <span className='border border-stroke-color w-[40px] h-[40px] flex justify-center items-center rounded-lg cursor-pointer'>
+                <Minus size={16} className='text-brand-color-light' /> 
+              </span>
+              <span className='text-color-primary'>1</span>
+              <span className='border border-brand-color-light w-[40px] h-[40px] flex justify-center items-center rounded-lg'>
+                <Plus size={16} className='text-brand-color-light' />
+              </span>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className='flex justify-between items-center gap-6'>
+          <Image src={cadeira} width={104} height={104} alt='Monitor Gamer' className='object-cover rounded-lg'/>
+          <div className='w-[232px] gap-6 flex flex-col'>
+            <div>
+              <p className='text-color-primary'>Cadeira Gamer  RGB - Preta com Iluminação (Led)</p>
+            </div>
+            <div className='flex justify-between items-center'>
+              <span className='text-color-primary'><strong>R$959,90</strong></span>
+              <span className='border border-stroke-color w-[40px] h-[40px] flex justify-center items-center rounded-lg'>
+                <Minus size={16} className='text-brand-color-light' /> 
+              </span>
+              <span className='text-color-primary'>1</span>
+              <span className='border border-brand-color-light w-[40px] h-[40px] flex justify-center items-center rounded-lg'>
+                <Plus size={16} className='text-brand-color-light' />
+              </span>
+            </div>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className='flex justify-between items-center gap-6'>
+          <Image src={teclado} width={104} height={104} alt='Monitor Gamer' className='object-cover rounded-lg'/>
+          <div className='w-[232px] gap-6 flex flex-col'>
+            <div>
+              <p className='text-color-primary'>Teclado Gamer Mecânico Low Profile RGB AW510K 580</p>
+            </div>
+            <div className='flex justify-between items-center'>
+              <span className='text-color-primary'><strong>R$1.002,00</strong></span>
+              <span className='border border-stroke-color w-[40px] h-[40px] flex justify-center items-center rounded-lg'>
+                <Minus size={16} className='text-brand-color-light' /> 
+              </span>
+              <span className='text-color-primary'>1</span>
+              <span className='border border-brand-color-light w-[40px] h-[40px] flex justify-center items-center rounded-lg'>
+                <Plus size={16} className='text-brand-color-light' />
+              </span>
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className='flex justify-between items-center gap-6'>
+          <Image src={headset} width={104} height={104} alt='Monitor Gamer' className='object-cover rounded-lg'/>
+          <div className='w-[232px] gap-6 flex flex-col'>
+            <div>
+              <p className='text-color-primary'>Teclado Gamer Mecânico Low Profile RGB AW510K 580</p>
+            </div>
+            <div className='flex justify-between items-center'>
+              <span className='text-color-primary'><strong>R$1.002,00</strong></span>
+              <span className='border border-stroke-color w-[40px] h-[40px] flex justify-center items-center rounded-lg'>
+                <Minus size={16} className='text-brand-color-light' /> 
+              </span>
+              <span className='text-color-primary'>1</span>
+              <span className='border border-brand-color-light w-[40px] h-[40px] flex justify-center items-center rounded-lg'>
+                <Plus size={16} className='text-brand-color-light' />
+              </span>
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        <div className='flex justify-between items-center gap-6'>
+          <Image src={patinho} width={104} height={104} alt='Monitor Gamer' className='object-cover rounded-lg'/>
+          <div className='w-[232px] gap-6 flex flex-col'>
+            <div>
+              <p className='text-color-primary'>Teclado Gamer Mecânico Low Profile RGB AW510K 580</p>
+            </div>
+            <div className='flex justify-between items-center'>
+              <span className='text-color-primary'><strong>R$1.002,00</strong></span>
+              <span className='border border-stroke-color w-[40px] h-[40px] flex justify-center items-center rounded-lg'>
+                <Minus size={16} className='text-brand-color-light' /> 
+              </span>
+              <span className='text-color-primary'>1</span>
+              <span className='border border-brand-color-light w-[40px] h-[40px] flex justify-center items-center rounded-lg'>
+                <Plus size={16} className='text-brand-color-light' />
+              </span>
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+      </main>
+
+      <footer className='h-[224px] p-8 grid gap-8'>
+       <div className='grid gap-4'>
+        <div className='flex justify-between item-center text-color-primary text-xl'>
+          <p>Total:</p>
+          <span><strong>R$10.681,69</strong></span>
+        </div>
+        <div className='flex justify-end items-center gap-2 text-brand-color-light'>
+          <Tag size={24}/>
+          <span>Adicionar cupom</span>
+        </div>
+       </div>
+       <div>
+        <button className='bg-brandColor w-[364px] h-16 rounded-lg text-color-primary'>Finalizar Compra</button>
+       </div>
+      </footer>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'brandColor': '#9333EA',
+        'brand-color-light': '#A855F7',
+        'brand-color-lighter': '#C084FC',
+        'color-primary': '#F9FAFB',
+        'color-secondary': '#E4E4E7',
+        'close-button-color': '#A1A1AA',
+        'stroke-color': '#3F3F46',
+        'surface-color': '#18181B',
+        'background-color': '#09090B'
+      }
     },
   },
   plugins: [],
